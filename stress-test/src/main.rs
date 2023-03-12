283dcbddaad9f41b3f4b20afb15c49e713d0dddc
Arthur Carcano <53921575+krtab@users.noreply.github.com>
2023-03-12 22:20:20 +0100

    improve bench a bit

--- stress-test/src/main.rs
+++ stress-test/src/main.rs
@@ -1,4 +1,22 @@
-fn f(id: String, n_child: usize, rem_depth: usize, b : rendezvous::Rendezvous) {
+use std::time::Instant;
+
+trait BarrierLike: Clone + Send {
+    fn wait(self);
+}
+
+impl BarrierLike for rendezvous::Rendezvous {
+    fn wait(self) {
+        rendezvous::Rendezvous::wait(self);
+    }
+}
+
+impl BarrierLike for adaptive_barrier::Barrier {
+    fn wait(mut self) {
+        adaptive_barrier::Barrier::wait(&mut self);
+    }
+}
+
+fn f(id: String, n_child: usize, rem_depth: usize, b: impl BarrierLike + 'static) {
     println!("{id}");
     if rem_depth == 0 {
         b.wait();
@@ -6,6 +24,7 @@ fn f(id: String, n_child: usize, rem_depth: usize, b : rendezvous::Rendezvous) {
     }
     for i in 0..n_child {
         let id_child = format!("{id}-{i}");
+        // let id_child = String::new();
         let b = b.clone();
         let _h = std::thread::spawn(move || f(id_child, n_child, rem_depth - 1, b));
         // handles.push(h);
@@ -14,7 +33,38 @@ fn f(id: String, n_child: usize, rem_depth: usize, b : rendezvous::Rendezvous) {
     b.wait();
 }
 
+fn g(id: String, n_child: usize, rem_depth: usize) {
+    // println!("{id}");
+    if rem_depth == 0 {
+        return;
+    }
+    let mut handles = Vec::new();
+    for i in 0..n_child {
+        let id_child = format!("{id}-{i}");
+        // let id_child = String::new();
+        let h = std::thread::spawn(move || g(id_child, n_child, rem_depth - 1));
+        handles.push(h);
+    }
+    for h in handles {
+        h.join().unwrap();
+    }
+}
+
 fn main() {
+    const N_CHILD: usize = 2;
+    const DEPTH: usize = 10;
     let b = rendezvous::Rendezvous::new();
-    f("".into(),2,5, b);
+    let start = Instant::now();
+    f("".into(), N_CHILD, DEPTH, b);
+    let end = start.elapsed();
+    eprintln!("rendez-vous: {}ms", end.as_millis());
+    let b = adaptive_barrier::Barrier::new(adaptive_barrier::PanicMode::Decrement);
+    let start = Instant::now();
+    f("".into(), N_CHILD, DEPTH, b);
+    let end = start.elapsed();
+    eprintln!("adaptive: {}ms", end.as_millis());
+    let start = Instant::now();
+    g("".into(), N_CHILD, DEPTH);
+    let end = start.elapsed();
+    eprintln!("join: {}ms", end.as_millis());
 }
